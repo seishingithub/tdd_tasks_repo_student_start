19a3987aa84ce298569546a6b48d2bbb4fc02021
Mike Gehard <mike.gehard@gmail.com>
2014-04-03 14:55:16 -0600

    Allow for creating of tasks
    can also list out all tasks

--- lib/tasks_repository.rb
+++ lib/tasks_repository.rb
@@ -1,3 +1,15 @@
-class TasksRepository
+require 'sequel'
 
+class TasksRepository
+  def initialize(db)
+    @tasks_table = db[:tasks]
+  end
+
+  def create(attributes)
+    @tasks_table.insert(attributes)
+  end
+
+  def all
+    @tasks_table.to_a
+  end
 end
--- spec/tasks_repository_spec.rb
+++ spec/tasks_repository_spec.rb
@@ -3,4 +3,27 @@ require 'spec_helper'
 require 'tasks_repository'
 
 describe TasksRepository do
+  let(:db) { Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test') }
+
+  before do
+    db.create_table :tasks do
+      primary_key :id
+      String :name
+    end
+  end
+
+  after do
+    db.drop_table :tasks
+  end
+
+  it 'allows for creating of a task' do
+    repo = TasksRepository.new(db)
+    repo.create({:name => "Do some stuff"})
+    repo.create({:name => "Do some other stuff"})
+    expected_tasks = [
+        {:id => 1, :name => "Do some stuff"},
+        {:id => 2, :name => "Do some other stuff"}
+    ]
+    expect(repo.all).to eq expected_tasks
+  end
 end
